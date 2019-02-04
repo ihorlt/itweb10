b9ae0a83d53c8f5be5a044b3f7c99a2a3b5bf5ba
Igor Lyutak <ihorlt@gmail.com>
2019-02-04 21:26:47 +0200

    add load fn

--- js/myApp.js
+++ js/myApp.js
@@ -2,5 +2,7 @@ $(document).ready(function(){
     $('h1').addClass('text-danger');
 
     // load menu
-    $('header').load('menu.html');
+    $('header').load('menu.html', function() {
+        console.log('After Menu');
+    });
     });
