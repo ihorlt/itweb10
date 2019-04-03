4998111b9b2366f23376f84c3db09582dc4d38f5
OlenaTaras <elena_taras@ukr.net>
2019-04-03 20:18:01 +0300

    added docs folder

---
+++ docs/js/myApp.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('h1').addClass('text-danger');
+
+    // load menu
+    $('header').load('menu.html', function() {
+        console.log('After Menu');
+
+        console.log('changed');
+        // dsfDSdsfDS
+    });
+    });
